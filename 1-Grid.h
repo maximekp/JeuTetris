179a8085b31fde5c8930c31de50e9c8fa0946509
maximekp <106911169+maximekp@users.noreply.github.com>
2022-06-12 14:53:48 +0200

    Add files via upload

---
+++ 1-Grid.h
@@ -0,0 +1,9 @@
+#ifndef GRID
+#define FRID
+
+#define SIZE 10
+
+
+void createGrid (char grid[][10], int size) ;
+
+#endif
---
+++ 2-Pieces.h
@@ -0,0 +1,12 @@
+#ifndef PIECES
+#define PIECES
+
+void createPieceO () ;
+void createPieceI () ;
+void createPieceT () ;
+void createPieceS () ;
+void createPieceZ () ;
+void createPieceL () ;
+void createPieceJ () ;
+
+#endif
